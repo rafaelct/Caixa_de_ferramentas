cd4c40b2d69c8f508cd2e2929fd0d2db70f0fb8c
Rafael Costa Teixeira <rafaelct@primeinformatica.com.br>
2020-08-04 19:57:56 -0300

    Adicionado contador dentro de uma conexão

--- websocket/server.js
+++ websocket/server.js
@@ -1,11 +1,16 @@
 
 const WebSocket = require('ws');
 
+
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+
+  let i = 0;
+
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    console.log(i++);
     ws.send('Maravilha');
   });
 
